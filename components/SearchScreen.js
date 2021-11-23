2bf3e7a69d37e2d609fdf0b48e072248d78baee3
Njeru Cyrus <njerucyrus5@hotmail.com>
2021-11-23 17:45:05 +0300

    cleaned up search page

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { View, StyleSheet, SafeAreaView, StatusBar, Text } from "react-native";
 import Focusawarestatusbar from "./FocusAwareStatusBar";
 import Recipedetailscreen from "./RecipeDetail/RecipeDetailScreen";
 
@@ -7,12 +7,18 @@ const Searchscreen = () => {
   return (
     <View style={styles.root}>
       <Focusawarestatusbar barStyle="light-content" />
+      <Text>This is search screen</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {},
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
 });
 
 export default Searchscreen;
